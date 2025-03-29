75fcfd63d5debeeb401c5f46b1b5bacf0c844f74
Alexander Eklund <alexander.eklund@arcticgroup.se>
2025-03-29 21:30:14 +0100

    feat: marcus ai

--- monster_ai.gd
+++ monster_ai.gd
@@ -1,1 +1,18 @@
-class_name MonsterAI extends Node2D
+extends Node2D
+class_name MonsterAI
+
+@export var speed: int = 100
+
+@onready var parent = get_parent()
+
+func _physics_process(delta: float) -> void:
+	var monster_position = parent.global_position
+	var direction_vec = (get_player_position() - monster_position).normalized()
+	parent.velocity = direction_vec * speed
+	parent.move_and_slide()
+
+
+func get_player_position() -> Vector2:
+	if Global.game_controller.player:
+		return Global.game_controller.player.global_position
+	return Vector2.ZERO
