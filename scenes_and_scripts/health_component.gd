0d9bd2ae833ed245bbca1f1d0557fd140f928461
Mehdows <Marcus99.ma@gmail.com>
2025-03-29 15:39:16 +0100

    wip hurt/hit boxes

---
+++ scenes_and_scripts/health_component.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+signal death()
+
+var health: int = 100:
+	set(amount):
+		if amount <= 0:
+			death.emit()
+		health = amount
+		print(health)
+	get:
+		return health
+var max_health: int = 100
+var regeneration: float = 1
+
+func _on_timer_timeout():
+	health += ceil(regeneration)
+	clamp(health, 0, max_health)
+
+func take_damage(damage):
+	health -= damage
