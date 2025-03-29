18f61aec1e9ccdef5aa1a584a79efc9b847afdea
Mehdows <Marcus99.ma@gmail.com>
2025-03-29 21:00:29 +0100

    feat: added enemy AI

--- resources/enemy_resource.gd
+++ resources/enemy_resource.gd
@@ -3,5 +3,6 @@ class_name EnemyResource
 
 @export var health: int = 20
 @export var attack: int = 20
+@export var speed: int = 50
 @export var shape: Shape2D = CircleShape2D.new()
 @export var sprite: SpriteFrames
