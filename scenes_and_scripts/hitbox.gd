8e0f78618d6df7163e03bf0000e7fb6d9facfbe7
Mehdows <Marcus99.ma@gmail.com>
2025-03-29 16:34:45 +0100

    hurt boxes and hit boxes detect

--- scenes_and_scripts/hitbox.gd
+++ scenes_and_scripts/hitbox.gd
@@ -2,8 +2,3 @@ class_name HitBox
 extends Area2D
 
 @export var damage: int
-@export var shape: Shape2D
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-
-func _ready() -> void:
-	collision_shape_2d.shape = shape
--- scenes_and_scripts/hurt_box.gd
+++ scenes_and_scripts/hurt_box.gd
@@ -2,11 +2,6 @@ class_name HurtBox
 extends Area2D
 
 #const hitEffectScene = preload("res://some_folder/hit_effect.tscn")
-@export var shape: Shape2D
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-
-func _ready() -> void:
-	collision_shape_2d.shape = shape
 
 func hit_effect():
 	#var hitEffect : Node2D = hitEffectScene.instantiate() as Node2D
